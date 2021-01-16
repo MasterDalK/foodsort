2a18acec82a9d4cfb2ef4ae1ba18d5c9e1b11786
MasterDalK <mastrdalk@gmail.com>
2021-01-16 12:48:44 -0500

    Initial Commit

---
+++ src/js/data/food.js
@@ -0,0 +1,80 @@
+dataSetVersion = "2021-01-16"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+];
+
+dataSet[dataSetVersion].characterData = [
+ {
+   name: "Burger King",
+   img: "bk.png"
+ },
+ {
+   name: "Meatpoint",
+   img: "meatpoint.png"
+ },
+ {
+   name: "Pho Mi Asia (Viet Place)",
+   img: "viet.png"
+ },
+ {
+   name: "Portugese",
+   img: "costaverde.png"
+ },
+ {
+   name: "Poke Bowls",
+   img: "poke.png"
+ },
+ {
+   name: "Swiss Chalet",
+   img: "swiss.png"
+ },
+ {
+   name: "Indian Food",
+   img: "indian.png"
+ },
+ {
+   name: "Pizza",
+   img: "pizza.png"
+ },
+ {
+   name: "A&W",
+   img: "aw.png"
+ },
+ {
+   name: "Hakka",
+   img: "hakka.png"
+ },
+ {
+   name: "Burritos",
+   img: "burrito.png"
+ },
+ {
+   name: "Subs",
+   img: "subs.png"
+ },
+ {
+   name: "Sushi",
+   img: "sushi.png"
+ },
+ {
+   name: "Fried Chicken",
+   img: "chicken.png"
+ },
+ {
+   name: "Diner Food",
+   img: "diner.png"
+ },
+ {
+   name: "McDonalds",
+   img: "mcd.png"
+ },
+ {
+   name: "Caribbean",
+   img: "Caribbean.png"
+ },
+ {
+   name: "Harvey's",
+   img: "harveys.png"
+ },
+];
